7494ec6dbdfda1e9c0bef0d2b667d715db28a1ee
stephvelazquez <svelazquez7700@gmail.com>
2025-07-29 21:41:13 -0700

    Format conditional table component render, add select onchange function

--- src/app/components/pages/Advocates.tsx
+++ src/app/components/pages/Advocates.tsx
@@ -4,19 +4,28 @@ import { Table } from "../shared/Table";
 import { Loading } from "../shared/Loading";
 import { EmptyState } from "../shared/EmptyState";
 import { Advocate } from "@/app/types/advocate-types";
+import { ItemPerPgSelect } from "../shared/ItemPerPgSelect";
+import { Pagination } from "../shared/Pagination";
 
 const COLUMN_NAMES = ['First Name', 'Last Name', 'City', 'Degree', 'Specialties', 'Years of Experience', 'Phone Number']
+
+const OPTIONS = ["All", 5, 10, 20]
 
 export const Advocates = () => {
   const [advocates, setAdvocates] = useState<undefined | Advocate[]>(undefined);
   const [filteredAdvocates, setFilteredAdvocates] = useState<undefined | Advocate[]>(undefined);
   const [showFilteredResults, setShowFilteredResults] = useState<boolean>(false)
   const [searchStr, setSearchStr] = useState<string>("")
+  const [currPage, setCurrPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [itemsPerPg, setItemsPerPg] = useState("All");
+  const [numAdvocates, setNumAdvocates] = useState<number>(0)
 
   useEffect(() => {
     fetch("/api/advocates").then((response) => {
       response.json().then((jsonResponse) => {
         setAdvocates(jsonResponse.data);
+        setNumAdvocates(jsonResponse.data.length)
       });
     });
   }, []);
@@ -49,11 +58,29 @@ export const Advocates = () => {
     });
 
     setFilteredAdvocates(filteredAdvocateArr)
+    if (filteredAdvocateArr?.length === 0) setNumAdvocates(0)
+    else setNumAdvocates(filteredAdvocateArr?.length as number)
     setShowFilteredResults(true)
   }
 
   const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
     if(e.key === "Enter") onSearchClick()
+  }
+  
+  const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+
+    if(value === "All") {
+      setFilteredAdvocates(advocates);
+    } else {
+      setFilteredAdvocates(advocates?.slice(0, 0 + Number(value)));
+    }
+    setItemsPerPg(value);
+    setCurrPage(1);
+  };
+
+  const onPaginationClick = () => {
+    // Do something
   }
 
   return (
@@ -62,7 +89,25 @@ export const Advocates = () => {
       <div style={{ marginBottom: '20px' }}>
         <SearchInput onSearchClick={onSearchClick} onInputChange={onChange} onResetClick={onResetClick} inputLabel="Search for" value={searchStr} onEnter={onEnter} inputDisabled={advocates === undefined} resetDisabled={searchStr === "" && !showFilteredResults} searchBtnDisabled={searchStr === ""} />
       </div>
-      {advocates === undefined ? <Loading /> : (showFilteredResults && filteredAdvocates && filteredAdvocates.length < 1) ? <EmptyState /> : <Table columnNames={COLUMN_NAMES} tableData={showFilteredResults ? filteredAdvocates as Advocate[] : advocates as Advocate[]} />}
+      <div>
+        {advocates === undefined
+          ? <Loading />
+          : (showFilteredResults && filteredAdvocates && filteredAdvocates.length < 1)
+            ? <EmptyState />
+            : <Table columnNames={COLUMN_NAMES} tableData={showFilteredResults
+              ? filteredAdvocates as Advocate[]
+              : advocates as Advocate[]}
+            />
+        }
+      </div>
+      <div>
+        <ItemPerPgSelect onChange={onSelectChange} options={OPTIONS} disabled={advocates === undefined} />
+        <Pagination
+          totalPages={totalPages}
+          onClick={onPaginationClick}
+          currPage={currPage}
+        />
+      </div>
     </div>
   )
 }
--- src/app/components/shared/ItemPerPgSelect.tsx
+++ src/app/components/shared/ItemPerPgSelect.tsx
@@ -3,12 +3,13 @@ import { ChangeEvent } from "react";
 interface SelectProps {
   options: Array<string | number>
   onChange: (e: ChangeEvent<HTMLSelectElement>) => void
+  disabled?: boolean
 }
 
-export const ItemPerPgSelect = ({ options, onChange }: SelectProps) => (
+export const ItemPerPgSelect = ({ options, onChange, disabled = false }: SelectProps) => (
   <div>
     <p>{"Display per page"}</p>
-    <select name="advocatesPerPg" onChange={onChange}>
+    <select name="advocatesPerPg" onChange={onChange} disabled={disabled} >
       {options.map((item, idx) => (
         <option value={item} key={`${item}-${idx}`}>
           {item}
