a592f099caeeed0258a24d7b811d0a830c29eb3b
stephvelazquez <svelazquez7700@gmail.com>
2025-07-31 11:51:04 -0700

    Refactor filteredAdvocates state to always be source of truth for data; remove showFilteredResults state; move numAdvocates state management to useEffect

--- src/app/components/pages/Advocates.tsx
+++ src/app/components/pages/Advocates.tsx
@@ -17,19 +17,17 @@ const OPTIONS = ["All", 5, 10, 20] // Remove `All` option with large data sets
 
 export const Advocates = () => {
   {/*
-    TODO: Consider if certain pieces of state can be managed in `useEffect` if they are consistently dependent on other state changes
-      - `totalPages` depends on `numAdvocates`, which in turn depends on `filteredAdvocates` and `showFilteredResults`
-      - `dataToDisplay` depends on `filteredAdvocates` and `showFilteredResults`
-    This might be cleaner than setting them in event handlers, but will that affect legibility positively or negatively?
-    Do we run the risk of some state update accidentally being omitted if we put that logic in handlers?
+    NOTE: Certain pieces of state are being managed in their own `useEffect` if they are consistently dependent on other state
+    changes (e.g. `numAdvocates`). This might be cleaner than setting them in event handlers, but does it affect legibility
+    positively or negatively? Is there a risk of some state updates accidentally being omitted if that logic is in handlers?
   */}
-  const [advocates, setAdvocates] = useState<undefined | Advocate[]>(undefined);
-  const [filteredAdvocates, setFilteredAdvocates] = useState<undefined | Advocate[]>(undefined);
-  const [showFilteredResults, setShowFilteredResults] = useState<boolean>(false)
+  const [advocates, setAdvocates] = useState<undefined | Advocate[]>(undefined)
+  const [filteredAdvocates, setFilteredAdvocates] = useState<undefined | Advocate[]>(undefined)
   const [searchStr, setSearchStr] = useState<string>("")
-  const [currPage, setCurrPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [itemsPerPg, setItemsPerPg] = useState<number | string>("All");
+  const [currPage, setCurrPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(1)
+  const [itemsPerPg, setItemsPerPg] = useState<number | string>("All")
+  // `dataToDisplay` stores data slice for current page when pagintion is enabled
   const [dataToDisplay, setDataToDisplay] = useState<undefined | Advocate[]>(undefined)
   const [numAdvocates, setNumAdvocates] = useState<number>(0) // TODO: Refactor API to send this information as part of response
 
@@ -38,10 +36,14 @@ export const Advocates = () => {
     fetch("/api/advocates").then((response) => {
       response.json().then((jsonResponse) => {
         setAdvocates(jsonResponse.data);
-        setNumAdvocates(jsonResponse.data.length)
+        setFilteredAdvocates(jsonResponse.data)
       });
     });
   }, []);
+
+  useEffect(() => {
+    if(filteredAdvocates !== undefined) setNumAdvocates(filteredAdvocates.length)
+  }, [filteredAdvocates])
 
   // Change total page count when user changes # of items per page
   useEffect(() => {
@@ -64,19 +66,16 @@ export const Advocates = () => {
 
 
   const onResetClick = () => {
-    setShowFilteredResults(false)
     setSearchStr("")
-    setFilteredAdvocates(undefined)
-    // Data should be defined by this point so ssertion as number will likely throw no errors
-    // NOTE: undefined data can be handled when implementing `Suspense` w/ fallback
-    setNumAdvocates(advocates?.length as number) 
+    setFilteredAdvocates(advocates)
   };
 
 
   const onSearchClick = () => {
-    const lowerCaseStr = searchStr.toLowerCase()
+    const lowerCaseStr = searchStr.toLowerCase() // Makes search case insensitive
 
     const filteredAdvocateArr = advocates?.filter(advocate => {
+      // NOTE: Could this be extracted into be a recursive helperfn that filters for matching substring?
       const filteredSpecialty = advocate.specialties.filter(item => item.toLowerCase().includes(lowerCaseStr))
 
       return filteredSpecialty.length > 0 ||
@@ -89,9 +88,6 @@ export const Advocates = () => {
     });
 
     setFilteredAdvocates(filteredAdvocateArr)
-    if (filteredAdvocateArr?.length === 0) setNumAdvocates(0)
-    else setNumAdvocates(filteredAdvocateArr?.length as number)
-    setShowFilteredResults(true)
   }
 
 
@@ -132,13 +128,13 @@ export const Advocates = () => {
     const currIdx = itemssPerPgToNum * currPage;
 
     const sliceMap: AdvocatesArrayObject = {
-      toFirst: dataToDisplay?.slice(0, itemssPerPgToNum), // TODO: refactor `filteredAdvoates` or add new state for pagination data
-      previous: dataToDisplay?.slice(
+      toFirst: filteredAdvocates?.slice(0, itemssPerPgToNum),
+      previous: filteredAdvocates?.slice(
         currIdx - itemssPerPgToNum * 2,
         currIdx - itemssPerPgToNum,
       ),
-      next: dataToDisplay?.slice(currIdx, currIdx + itemssPerPgToNum),
-      toLast: dataToDisplay?.slice(numAdvocates - itemssPerPgToNum),
+      next: filteredAdvocates?.slice(currIdx, currIdx + itemssPerPgToNum),
+      toLast: filteredAdvocates?.slice(numAdvocates - itemssPerPgToNum),
     };
 
     setDataToDisplay(sliceMap[name]);
@@ -149,20 +145,18 @@ export const Advocates = () => {
     <div>
       <h1 style={{ fontSize: '50px',  fontWeight: '600', marginBottom: '3rem'}}>Solace Advocates</h1>
       <div style={{ marginBottom: '2rem' }}>
-        <SearchInput onSearchClick={onSearchClick} onInputChange={onInputChange} onResetClick={onResetClick} inputLabel="Search for" value={searchStr} onEnter={onEnter} inputDisabled={advocates === undefined} resetDisabled={searchStr === "" && !showFilteredResults} searchBtnDisabled={searchStr === ""} />
+        <SearchInput onSearchClick={onSearchClick} onInputChange={onInputChange} onResetClick={onResetClick} inputLabel="Search advocates" placeholder="Looking for something specific?" value={searchStr} onEnter={onEnter} inputDisabled={advocates === undefined} resetDisabled={searchStr === "" && filteredAdvocates === undefined} searchBtnDisabled={searchStr === ""} />
       </div>
       <div>
-        {advocates === undefined
+        {filteredAdvocates === undefined
           ? <Loading />
-          : (showFilteredResults && filteredAdvocates && filteredAdvocates.length < 1)
+          : (filteredAdvocates && filteredAdvocates.length < 1)
             ? <EmptyState />
-            : <Table columnNames={COLUMN_NAMES} tableData={showFilteredResults
-              ? filteredAdvocates as Advocate[]
-              : advocates as Advocate[]}
+            : <Table columnNames={COLUMN_NAMES} tableData={dataToDisplay as Advocate[]}
             />
         }
       </div>
-      {/* Extract common styles into shared object */}
+      {/* TODO: Extract common styles into shared object */}
       <div style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', marginTop: '3rem'}}>
         <ItemPerPgSelect onChange={onSelectChange} options={OPTIONS} disabled={advocates === undefined} />
         <Pagination
--- src/app/components/shared/SearchInput.tsx
+++ src/app/components/shared/SearchInput.tsx
@@ -7,15 +7,16 @@ interface SearchInputProps {
   onSearchClick: () => void
   onEnter: (e: KeyboardEvent<HTMLInputElement>) => void
   value: string
+  placeholder?: string
   inputDisabled: boolean
   resetDisabled: boolean
   searchBtnDisabled: boolean
 }
 
-export const SearchInput = ({inputLabel, onResetClick, onInputChange, onSearchClick, onEnter, value, inputDisabled, resetDisabled, searchBtnDisabled}: SearchInputProps) => (
+export const SearchInput = ({inputLabel, onResetClick, onInputChange, onSearchClick, onEnter, value, placeholder, inputDisabled, resetDisabled, searchBtnDisabled}: SearchInputProps) => (
   <div>
     <label htmlFor="search">{inputLabel}</label>
-    <input name="search" onChange={onInputChange} onKeyDown={onEnter} value={value} disabled={inputDisabled} style={{ border: 'solid', margin: '10px', borderRadius: '5px' }} />
+    <input name="search" onChange={onInputChange} onKeyDown={onEnter} value={value} placeholder={placeholder} disabled={inputDisabled} style={{ border: 'solid', margin: '10px', borderRadius: '5px' }} />
     <button onClick={onSearchClick} disabled={searchBtnDisabled} style={{border: 'solid', paddingLeft: '5px', paddingRight: '5px', marginRight: '5px', borderRadius: '5px'}}>Search</button>
     <button onClick={onResetClick} disabled={resetDisabled} style={{border: 'solid', paddingLeft: '5px', paddingRight: '5px', borderRadius: '5px', background: '#d3d3d3'}}>Reset</button>
   </div>
