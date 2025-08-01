7212f0dd88084b6a001ba0456764d262bd365e8a
stephvelazquez <svelazquez7700@gmail.com>
2025-08-01 13:41:21 -0700

    Update unique key for nested array in table data

--- src/app/components/shared/Table.tsx
+++ src/app/components/shared/Table.tsx
@@ -10,9 +10,6 @@ interface TableProps {
 export const Table = ({columnNames, tableData}: TableProps) => (
   <table>
     <thead style={{borderBottom: '2px solid'}}>
-      {/* 
-        <th> cannot be direct child of <thead> -- need to wrap in <tr>
-      */}
       <tr>
         {columnNames.map(name => (
           <th key={`${name}`}>{name}</th>
@@ -20,21 +17,23 @@ export const Table = ({columnNames, tableData}: TableProps) => (
       </tr>
     </thead>
     <tbody>
-      {tableData.map((dataObj) => (
-        <tr key={crypto.randomUUID()} style={{ borderBottom: 'solid #d3d3d3' }}> {/** Use key generator since data can contain arrays */}
-          {Object.keys(dataObj).map((datumKey, ind) => {
-            const datumVal = dataObj[datumKey]
-            const datumIsArr = Array.isArray(datumVal)
-            return (
-              <td key={datumIsArr ? datumKey : datumVal.toString()}> {/** Cast value as string in case `datumVal` is type boolean */}
-                {datumIsArr
-                ? datumVal.map(item => (
-                  <p>{item}</p>
-                ))
-                : `${datumVal}`}
-              </td>
-            )
-          })}
+      {tableData.map((dataObj) => {
+        const uniqueKey = crypto.randomUUID()
+        return (
+          <tr key={uniqueKey} style={{ borderBottom: 'solid #d3d3d3' }}> {/** Use key generator since data can contain arrays */}
+            {Object.keys(dataObj).map((datumKey, ind) => {
+              const datumVal = dataObj[datumKey]
+              const datumIsArr = Array.isArray(datumVal)
+              return (
+                <td key={datumIsArr ? `${uniqueKey}-${datumKey}` : datumVal.toString()}> {/** Cast value as string in case `datumVal` is type boolean */}
+                  {datumIsArr
+                    ? datumVal.map(item => (
+                      <p>{item}</p>
+                    ))
+                    : `${datumVal}`}
+                </td>
+              )
+            })}
             {/* <td>{advocate.firstName}</td>
               <td>{advocate.lastName}</td>
               <td>{advocate.city}</td>
@@ -46,8 +45,9 @@ export const Table = ({columnNames, tableData}: TableProps) => (
               </td>
               <td>{advocate.yearsOfExperience}</td>
               <td>{formatPhoneNumber(advocate.phoneNumber)}</td> */}
-        </tr>
-      ))}
+          </tr>
+        )
+      })}
     </tbody>
   </table>
 )
